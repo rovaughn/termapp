e6623ce88306a4babff68948af747676f5966293
Alec Newman <alecnwmn904@gmail.com>
2018-12-28 15:25:07 -0700

    When printing, don't wrap around

--- terminal.go
+++ terminal.go
@@ -72,7 +72,11 @@ func (s *Screen) SetCursor(x, y int, visible bool) {
 
 func (s *Screen) Print(x, y int, back, fore Color, text string) {
 	start := y*s.width + x
+	end := (y + 1) * s.width
 	for i, r := range text {
+		if start+i >= end {
+			break
+		}
 		s.cells[start+i] = Cell{
 			Style: Style{
 				back: back,
